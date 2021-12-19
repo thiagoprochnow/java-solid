4c8ae7fd12f2fc308231944c93c64c13756fffbf
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-12-19 15:07:26 -0300

    Extract validations from service class

--- br/com/alura/rh/service/ReajusteService.java
+++ br/com/alura/rh/service/ReajusteService.java
@@ -3,20 +3,24 @@ package br.com.alura.rh.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 public class ReajusteService {
 
-    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual , RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
+    private List<ValidacaoReajuste> validacoes;
+    
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {        
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
         
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
 }
